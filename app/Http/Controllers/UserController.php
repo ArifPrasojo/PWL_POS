20dbaccebce51eb378639121dbcac2f64a079217
username Anda <arifprasojo999@email.com>
2024-09-20 08:14:27 +0700

    Praktikum 1 - Pertemuan 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,21 +9,20 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('level_id', '2')->update($data); // Mengupdate data user
-
-        $user = UserModel::all(); // Mengambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
-
         // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 2
+        //     'nama' => 'Pelanggan Pertama',
         // ];
-        // UserModel::insert($data);
-        
+        // UserModel::where('level_id', '2')->update($data); // Mengupdate data user
+
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
 }
