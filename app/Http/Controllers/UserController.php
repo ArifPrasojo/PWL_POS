cdb14c9c284242435c1ab5b9d52147bf7b729029
username Anda <arifprasojo999@email.com>
2024-09-20 10:06:10 +0700

    Praktikum 2.2 - Pertemuan 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,15 +14,27 @@ class UserController extends Controller
         // ];
         // UserModel::where('level_id', '2')->update($data); // Mengupdate data user
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::firstwhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('level_id', 1)->firstOrFail();
+        // return view('user', ['data' => $user]);
+
+        
+
     }
 }
